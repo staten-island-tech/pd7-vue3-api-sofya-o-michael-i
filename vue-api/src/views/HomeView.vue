5952422eb55dccb2003ebbc85548413ce0a8564d
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-04-24 13:14:00 -0400

    4/24

--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -4,7 +4,14 @@
 
 <script setup>
 // https://data.cityofnewyork.us/resource/8m42-w767.json
-//tsjhth
+import { ref, onMounted } from 'vue'
+const fireDept = ref('')
+async function emsData() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/8m42-w767.json')
+  let data = await res.json()
+  fireDept.value = data
+}
+onMounted(() => emsData())
 </script>
 
 <style lang="scss" scoped></style>
