40e8d265c6a22a2417b1e0f3b3c32cc9b52617fc
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-05-02 00:22:55 -0400

    5/2

--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -1,75 +1,9 @@
 <template>
-  <div class="content">
-    <h1>Select type of Emergencies</h1>
-    <select name="" id="">
-      <option value="Default">Default</option>
-      <option value="Medical">Medical</option>
-      <option value="Non-Medical">Non-Mecical</option>
-      <option value="Structural Fires">Structural Fires</option>
-    </select>
-    <div class="chart">
-      <DoughnutChart v-if="loaded" :data="chartData" :options="chartOptions" />
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import DoughnutChart from '../components/DoughnutChart.vue'
-export default {
-  name: 'DoughnutView',
-  components: {
-    DoughnutChart
-  },
-  data() {
-    return {
-      loaded: false,
-      chartData: {
-        labels: [],
-        datasets: []
-      },
-      chartOptions: {}
-    }
-  },
-  methods: {
-    async boroughData() {
-      try {
-        const res = await fetch('https://data.cityofnewyork.us/resource/8m42-w767.json')
-        const data = await res.json()
-        let boroughType = []
-        let labels = ['Manhattan', 'Brooklyn']
-        let backCol = ['#cdb4db', '#ffc8dd']
-        labels.forEach((label) => {
-          boroughType.push(data.filter((results) => results.alarm_box_borough == label).length)
-        })
-        this.chartData = {
-          labels: labels,
-          datasets: [
-            {
-              data: boroughType,
-              backgroundColor: backCol
-            }
-          ]
-        }
-        this.loaded = true
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    // async optionsSelect() {
-    //   try {
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // }
-  },
-  async mounted() {
-    this.boroughData()
-  }
-}
+export default {}
 </script>
 
-<style scoped>
-.chart {
-  width: 40rem;
-}
-</style>
+<style lang="scss" scoped></style>
